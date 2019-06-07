c371803a481584c5e4e40728afc698555f68f704
Ayush Shekhar <ayush221b@gmail.com>
2019-06-07 12:41:13 +0530

    Changed model

--- controllers/image.js
+++ controllers/image.js
@@ -3,31 +3,33 @@ const Clarifai = require('clarifai');
 require('dotenv').config()
 
 const app = new Clarifai.App({
- apiKey: process.env.API_KEY
+	apiKey: process.env.API_KEY
 });
 
-const handleApiCall = (req,res) => {
+const handleApiCall = (req, res) => {
 	app.models
 		.predict(e466caa0619f444ab97497640cefc4dc, req.body.input)
-		.then(data=> {
+		.then(data => {
 			res.json(data);
 		})
-		.catch(err=> res.status(400).json(err))
+		.catch(err => res.status(400).json(err))
 
 }
 
-const handleImage = (req,res,db) => {
-	const {id} = req.body;
-	db('users').where('id','=', id)
-	.increment('entries',1)
-	.returning('entries')
-	.then(entries => {
-		res.json(entries[0]);
-	})
-	.catch(err=> res.status(400).json('unable to get entries'))
+const handleImage = (req, res, db) => {
+	const {
+		id
+	} = req.body;
+	db('users').where('id', '=', id)
+		.increment('entries', 1)
+		.returning('entries')
+		.then(entries => {
+			res.json(entries[0]);
+		})
+		.catch(err => res.status(400).json('unable to get entries'))
 }
 
-module.exports= {
+module.exports = {
 	handleImage,
 	handleApiCall
 }
